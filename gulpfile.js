af954e0976ee8fde74cf9539ae36ca2a1913562f
sexfat <bryant6831@gmail.com>
2022-12-20 10:40:00 +0800

    add async

--- gulpfile.js
+++ gulpfile.js
@@ -11,5 +11,47 @@ function defaultTask(cb){
   cb();
 }
 
+
+function TaskA(cb){
+    console.log('A');
+    cb();
+  }
+
+
+function TaskB(cb){
+    console.log('B');
+    cb();
+}
+
+function TaskC(cb){
+    console.log('C');
+    cb();
+}
+
+function TaskD(cb){
+    console.log('D');
+    cb();
+}
+
+function TaskE(cb){
+    console.log('E');
+    cb();
+}
+
+// a -> b 
+exports.async = series(TaskA , TaskB);
+
+// c , d  -> finish
+exports.sync = parallel(TaskC , TaskD);
+
+exports.all = series(TaskA, TaskB , parallel(TaskC , TaskD) , TaskE)
+
+
+
+
+
+
+
+
 exports.do = defaultTask
 
