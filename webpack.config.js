84688733a3ab28554c6f71d8062cae137a7fd664
sexfat <bryant6831@gmail.com>
2022-12-28 15:18:16 +0800

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,90 +1,89 @@
-const path = require('path');
-const webpack  = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {
-    CleanWebpackPlugin
-} = require('clean-webpack-plugin');
-
-
-
-
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: {
-        index :'./src/index.js',
-     // about : './path/'
-    },               // 入口文件 
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },              // 出口文件
+  entry: {
+    index: "./src/index.js",
+    // about : './path/'
+  }, // 入口文件
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  }, // 出口文件
 
-    module: {
-        rules: [{
-            // 格式
-            test: /\.(sass|scss|css)$/,
-            //順序是由下到上 css > style
-            use: [{
-                    loader: MiniCssExtractPlugin.loader,
-                    options: {
-                      publicPath: './dist'
-                    }
-                  },
-                // 'style-loader',//跟MiniCssExtractPlugin 會衝突所以要關掉
-                'css-loader',
-                'sass-loader'
-            ],
-        },
-        //babel loader
-        {
-            test: /\.(js)$/,
-            exclude: /(node_modules)/,
+  module: {
+    rules: [
+      {
+        // 格式
+        test: /\.(sass|scss|css)$/,
+        //順序是由下到上 css > style
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./dist",
+            },
+          },
+          // 'style-loader',//跟MiniCssExtractPlugin 會衝突所以要關掉
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      //babel loader
+      {
+        test: /\.(js)$/,
+        exclude: /(node_modules)/,
 
-            use: [{
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-            }],
-            include: path.resolve(__dirname, 'src'),
-        },
-
-      ]
-
-    },              // 處裡對應模組
-    plugins: [
-        //清理舊的檔案
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: "./[name].css" //產生 css
-        }),
-        new HtmlWebpackPlugin({
-            chunks : ['index'],  //選擇注入資源 chunk
-            inject  : 'body', //預設<body> js </body>  head or body
-            template : './src/index.html',
-            //來源
-            filename : 'index.html'
-            // 目的地
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery'
-          })
-
-    ],               // 對應的插件
-    devServer: {
-        contentBase: './dist',
-        host: 'localhost',
-        port: 3000,
-        // 指定首頁檔案
-        index: 'index.html',
-        open: true
-    },          // 服務器配置
-    mode: 'development',      // 開發模式配置
-    resolve: {
-        alias: {
-            vue: 'vue/dist/vue.esm-bundler.js'
-        }
-    }
-}
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        ],
+        include: path.resolve(__dirname, "src"),
+      },
+    ],
+  }, // 處裡對應模組
+  plugins: [
+    //清理舊的檔案
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "./[name].css", //產生 css
+    }),
+    new HtmlWebpackPlugin({
+      chunks: ["index"], //選擇注入資源 chunk
+      inject: "body", //預設<body> js </body>  head or body
+      template: "./src/index.html",
+      //來源
+      filename: "index.html",
+      // 目的地
+    }),
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+    }),
+    new webpack.DefinePlugin({
+      __VUE_OPTIONS_API__: true,
+      __VUE_PROD_DEVTOOLS__: true,
+    }),
+  ], // 對應的插件
+  devServer: {
+    contentBase: "./dist",
+    host: "localhost",
+    port: 3000,
+    // 指定首頁檔案
+    index: "index.html",
+    open: true,
+  }, // 服務器配置
+  mode: "development", // 開發模式配置
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
+  },
+};
