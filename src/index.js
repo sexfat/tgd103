8255a1837e2e7faffba4340f11b65e60b6e3de52
sexfat <bryant6831@gmail.com>
2022-12-28 10:01:07 +0800

    add webpack

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+$('body').css('background-color' , 'green');
+
+
+//gsap 
+import { gsap } from "gsap";
+
+
+
+
+
+// test
+function aa(x){
+  return  x * 10 
+}
+
+console.log(aa(20));
+console.log('finish');
+
+
+
